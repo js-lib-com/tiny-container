0aa8038326f43578266df7c598766fd97f3764a8
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2021-11-26 21:15:24 +0200

    Move CDI module to container core.

--- tiny-core/src/main/java/js/tiny/container/cdi/ContainerModule.java
+++ tiny-core/src/main/java/js/tiny/container/cdi/ContainerModule.java
@@ -6,7 +6,7 @@ import java.util.List;
 import js.injector.AbstractModule;
 import js.injector.IBindingBuilder;
 
-public class ContainerModule extends AbstractModule {
+class ContainerModule extends AbstractModule {
 
 	private final List<ContainerBinding<?>> bindings = new ArrayList<>();
 
